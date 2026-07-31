856110a6868d828ea1b56cd245777e37b6ba333d
ChartresComm <communication@agglo-ville.chartres.fr>
2026-07-31 02:49:49 +0200

    Add files via upload

--- js/client.js
+++ js/client.js
@@ -3,6 +3,11 @@
 
 /* global TrelloPowerUp */
 
+// IMPORTANT : les icônes des board-buttons doivent être en URL ABSOLUE.
+// Le bouton est rendu directement dans la page Trello (pas dans notre iframe),
+// donc un chemin relatif ('./icons/...') se résout par rapport à trello.com
+// et non par rapport à notre hébergement GitHub Pages -> icône introuvable,
+// sans erreur console (juste le texte "CDF" reste visible, sans image).
 var ICON = {
   dark: 'https://chartrescomm.github.io/chemin-de-fer-powerup/icons/icon-white-16.png',
   light: 'https://chartrescomm.github.io/chemin-de-fer-powerup/icons/icon-color-16.png'
@@ -17,7 +22,7 @@ TrelloPowerUp.initialize({
   'board-buttons': function (t, options) {
     return [{
       icon: ICON,
-      text: 'CDF',
+      text: 'Chemin de Fer',
       callback: function (t) {
         return t.modal({
           url: './chemin-de-fer.html',
